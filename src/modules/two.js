8de046499305dd0f1ccee08a546b90832b917592
rus733 <9282716617@mail.ru>
2022-01-14 12:32:13 +0300

    add: diploms files

--- src/modules/two.js
+++ src/modules/two.js
@@ -2,7 +2,7 @@ const twoModule = () => {
   const two = 'two';
   console.log(two);
   console.log(' two запуск в среде devServer ');
-  console.log(' two проверка после переноса main.js в js');
+  console.log(' two проверка после переноса main.js в js'); //
 };
 
 export default twoModule;
