4f1a0c48644c4a0bbb44d3234832753ad7fb2501
rus733 <9282716617@mail.ru>
2022-01-18 11:05:35 +0300

    add new  sendForm

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -3,80 +3,70 @@
 import { unBlockBody } from './helpers';
 
 const sendForm = () => {
-  const sendData = (data) => {
-    return fetch('./server.php', {
+  const errorMessage = 'Что-то пошло не так...',
+    successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+  const forms = document.querySelectorAll('form');
+  const modalOverlay = document.querySelector('.modal-overlay'),
+    responseMessageModal = document.getElementById('responseMessage'),
+    modalContent = responseMessageModal.querySelector('.modal-content'),
+    load = responseMessageModal.querySelector('.spinner'),
+    closeBtn = responseMessageModal.querySelector('.fancyClose');
+  const statusMessage = document.createElement('div');
+  statusMessage.style.cssText = `font-size: 2rem`;
+
+  const postData = async (data, cb) => {
+    const response = await fetch('./server.php', {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify(data),
-      headers: { 'Content-Type': 'application/json' },
     });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch`);
+    }
+    cb(response.status);
   };
 
-  const submitForm = (form) => {
-    const statusMessage = document.createElement('div');
+  const closedWindow = () => {
+    responseMessageModal.style.display = 'none';
+    modalOverlay.style.display = 'none';
+    statusMessage.remove();
+    closeBtn.removeEventListener('click', closedWindow);
+  };
 
-    const showStatus = (status) => {
-      const img = document.createElement('img');
-      const statusList = {
-        load: {
-          message: ' Загрузка...',
-          img: './images/message/waiting.gif',
-        },
-        error: {
-          message: ' Что-то пошло не так...',
-          img: './images/message/error.png',
-        },
-        success: {
-          message: ' Спасибо. Наш менеджер скоро с вами свяжемся!',
-          img: './images/message/success.png',
-        },
+  forms.forEach((form) => {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const modalWindow = form.closest('.modal');
+      modalWindow.style.display = 'none';
+      modalOverlay.style.display = 'none';
+      responseMessageModal.style.display = 'block';
+      load.style.display = 'block';
+
+      const formData = new FormData(form);
+      const data = {};
+      formData.forEach((value, key) => (data[key] = value));
+
+      const addStatusMessage = (message) => {
+        load.style.display = 'none';
+        modalContent.append(statusMessage);
+        if (message === errorMessage) statusMessage.style.color = 'red';
+        if (message === successMessage) statusMessage.style.color = '#2fab6d';
+        statusMessage.textContent = message;
+        closeBtn.addEventListener('click', closedWindow);
+        form.reset();
       };
 
-      statusMessage.textContent = statusList[status].message;
-      img.src = statusList[status].img;
-      img.height = 35;
-
-      statusMessage.insertBefore(img, statusMessage.firstChild);
-    };
-
-    statusMessage.style.cssText = 'font-size: 2rem; color: #000';
-
-    form.addEventListener('submit', (e) => {
-      const formElements = form.querySelectorAll('input'),
-        formData = new FormData(form),
-        formBody = {};
-      e.preventDefault();
-
-      showStatus('load');
-      form.appendChild(statusMessage);
-
-      formData.forEach((val, key) => {
-        formBody[key] = val;
+      postData(data, () => {
+        addStatusMessage(successMessage);
+      }).catch((error) => {
+        addStatusMessage(errorMessage);
+        console.error(error);
       });
-
-      sendData(formBody)
-        .then((data) => {
-          showStatus('success');
-
-          formElements.forEach((input) => {
-            input.value = '';
-            document.querySelectorAll('input[type="submit"]').forEach((btn) => {
-              btn.value = 'Отправить';
-            });
-          });
-          form.style.paddingLeft = '10px';
-          setTimeout(() => {
-            document.querySelector('.modal-callback').style.display = 'none';
-            document.querySelector('.modal-overlay').style.display = 'none';
-            unBlockBody();
-          }, 3500);
-        })
-        .catch((error) => {
-          showStatus('error');
-        });
     });
-  };
-
-  document.querySelectorAll('form').forEach((elem) => submitForm(elem));
+  });
 };
 
 export default sendForm;
